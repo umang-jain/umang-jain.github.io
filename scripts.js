f27c00196c9bbd057032ad519fa5df7047622a5b
umang-jain <umang.usict.042164@ipu.ac.in>
2019-03-31 09:57:09 +0530

    add bubble effect

---
+++ scripts.js
@@ -0,0 +1,170 @@
+window.onload = setTimeout(function() {
+
+  let messagesEl = document.querySelector('.messages');
+  let typingSpeed = 20;
+  let loadingText = '<b>•</b><b>•</b><b>•</b>';
+  let messageIndex = 0;
+
+  let getCurrentTime = function() {
+    let date = new Date();
+    let hours =  date.getHours();
+    let minutes =  date.getMinutes();
+    let current = hours + (minutes * .01);
+    if (current >= 5 && current < 19) return 'Have a nice day';
+    if (current >= 19 && current < 22) return 'Have a nice evening';
+    if (current >= 22 || current < 5) return 'Have a good night';
+  };
+
+  let messages = [
+    'Hey there 👋',
+    'I\'m Umang',
+    'A 3rd Year Computer Science Student at USICT, GGSIPU',
+    'And a Full Stack Web Developer',
+    'I\'m currently looking for internship opportunities in summer vacations.<br> You can contact me at <a href="mailto:umang.usict.042164@ipu.ac.in">umang.usict.042164@ipu.ac.in</a>',
+    '<a target="_blank" href="https://linkedin.com/in/shubhamg931">linkedin.com/in/shubhamg931</a><br><a target="_blank" href="https://github.com/shubhamg931">github.com/shubhamg931</a><br><a target="_blank" href="https://www.stopstalk.com/user/profile/shubhamg931">stopstalk.com/user/profile/shubhamg931</a><br><a target="_blank" href="https://twitter.com/shubhamg931">twitter.com/shubhamg931</a>',
+    'In case you are a Recruiter ➡️ <a target="_blank" href="UmangResume.pdf">Résumé</a>📝 ',
+    getCurrentTime(),
+    '👀 umang-jain.',
+  ];
+
+  let getFontSize = function() {
+    return parseInt(getComputedStyle(document.body).getPropertyValue('font-size'));
+  };
+
+  let pxToRem = function(px) {
+    return px / getFontSize() + 'rem';
+  };
+
+  let createBubbleElements = function(message, position) {
+    let bubbleEl = document.createElement('div');
+    let messageEl = document.createElement('span');
+    let loadingEl = document.createElement('span');
+    bubbleEl.classList.add('bubble');
+    bubbleEl.classList.add('is-loading');
+    bubbleEl.classList.add('cornered');
+    bubbleEl.classList.add(position === 'right' ? 'right' : 'left');
+    messageEl.classList.add('message');
+    loadingEl.classList.add('loading');
+    messageEl.innerHTML = message;
+    loadingEl.innerHTML = loadingText;
+    bubbleEl.appendChild(loadingEl);
+    bubbleEl.appendChild(messageEl);
+    bubbleEl.style.opacity = 0;
+    return {
+      bubble: bubbleEl,
+      message: messageEl,
+      loading: loadingEl
+    }
+  };
+
+  let getDimentions = function(elements) {
+    return dimensions = {
+      loading: {
+        w: '4rem',
+        h: '2.25rem'
+      },
+      bubble: {
+        w: pxToRem(elements.bubble.offsetWidth + 4),
+        h: pxToRem(elements.bubble.offsetHeight)
+      },
+      message: {
+        w: pxToRem(elements.message.offsetWidth + 4),
+        h: pxToRem(elements.message.offsetHeight)
+      }
+    }
+  };
+
+  let sendMessage = function(message, position) {
+    let loadingDuration = (message.replace(/<(?:.|\n)*?>/gm, '').length * typingSpeed) + 500;
+    let elements = createBubbleElements(message, position);
+    messagesEl.appendChild(elements.bubble);
+    messagesEl.appendChild(document.createElement('br'));
+    let dimensions = getDimentions(elements);
+    elements.bubble.style.width = '0rem';
+    elements.bubble.style.height = dimensions.loading.h;
+    elements.message.style.width = dimensions.message.w;
+    elements.message.style.height = dimensions.message.h;
+    elements.bubble.style.opacity = 1;
+    let bubbleOffset = elements.bubble.offsetTop + elements.bubble.offsetHeight;
+    if (bubbleOffset > messagesEl.offsetHeight) {
+      let scrollMessages = anime({
+        targets: messagesEl,
+        scrollTop: bubbleOffset,
+        duration: 750
+      });
+    }
+    let bubbleSize = anime({
+      targets: elements.bubble,
+      width: ['0rem', dimensions.loading.w],
+      marginTop: ['2.5rem', 0],
+      marginLeft: ['-2.5rem', 0],
+      duration: 800,
+      easing: 'easeOutElastic'
+    });
+    let loadingLoop = anime({
+      targets: elements.bubble,
+      scale: [1.05, .95],
+      duration: 1100,
+      loop: true,
+      direction: 'alternate',
+      easing: 'easeInOutQuad'
+    });
+    let dotsStart = anime({
+      targets: elements.loading,
+      translateX: ['-2rem', '0rem'],
+      scale: [.5, 1],
+      duration: 400,
+      delay: 25,
+      easing: 'easeOutElastic',
+    });
+    let dotsPulse = anime({
+      targets: elements.bubble.querySelectorAll('b'),
+      scale: [1, 1.25],
+      opacity: [.5, 1],
+      duration: 300,
+      loop: true,
+      direction: 'alternate',
+      delay: function(i) {return (i * 100) + 50}
+    });
+    setTimeout(function() {
+      loadingLoop.pause();
+      dotsPulse.restart({
+        opacity: 0,
+        scale: 0,
+        loop: false,
+        direction: 'forwards',
+        update: function(a) {
+          if (a.progress >= 65 && elements.bubble.classList.contains('is-loading')) {
+            elements.bubble.classList.remove('is-loading');
+            anime({
+              targets: elements.message,
+              opacity: [0, 1],
+              duration: 300,
+            });
+          }
+        }
+      });
+      bubbleSize.restart({
+        scale: 1,
+        width: [dimensions.loading.w, dimensions.bubble.w ],
+        height: [dimensions.loading.h, dimensions.bubble.h ],
+        marginTop: 0,
+        marginLeft: 0,
+        begin: function() {
+          if (messageIndex < messages.length) elements.bubble.classList.remove('cornered');
+        }
+      })
+    }, loadingDuration - 50);
+  };
+
+  let sendMessages = function() {
+    let message = messages[messageIndex];
+    if (!message) return;
+    sendMessage(message);
+    ++messageIndex;
+    setTimeout(sendMessages, (message.replace(/<(?:.|\n)*?>/gm, '').length * typingSpeed) + anime.random(900, 1200));
+  };
+
+  sendMessages();
+
+},5000);
